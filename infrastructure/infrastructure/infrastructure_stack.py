60a8a8ac073f2d4f57564d5f7803364f6ff6645c
Gabriel <garoga@amazon.com>
2022-10-10 22:25:51 +0200

    code adjustment after first review

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -20,34 +20,8 @@ account_id = sts.get_caller_identity()["Account"]
 LATEST_PYTORCH_VERSION = "1.8.1"
 LATEST_TRANSFORMERS_VERSION = "4.10.2"
 
-region_dict = {
-    "af-south-1": "626614931356",
-    "ap-east-1": "871362719292",
-    "ap-northeast-1": "763104351884",
-    "ap-northeast-2": "763104351884",
-    "ap-northeast-3": "364406365360",
-    "ap-south-1": "763104351884",
-    "ap-southeast-1": "763104351884",
-    "ap-southeast-2": "763104351884",
-    "ca-central-1": "763104351884",
-    "cn-north-1": "727897471807",
-    "cn-northwest-1": "727897471807",
-    "eu-central-1": "763104351884",
-    "eu-north-1": "763104351884",
-    "eu-south-1": "692866216735",
-    "eu-west-1": "763104351884",
-    "eu-west-2": "763104351884",
-    "eu-west-3": "763104351884",
-    "me-south-1": "217643126080",
-    "sa-east-1": "763104351884",
-    "us-east-1": "763104351884",
-    "us-east-2": "763104351884",
-    "us-gov-west-1": "442386744353",
-    "us-iso-east-1": "886529160074",
-    "us-west-1": "763104351884",
-    "us-west-2": "763104351884",
-}
-
+with open("infrastructure/region_dict.json", "r") as read_content:
+    region_dict=json.load(read_content)
 
 def get_image_uri(
     region=None,
@@ -123,8 +97,10 @@ class InfrastructureStack(Stack):
         )
 
         email_list = emails.value_as_list
+        print("HEREE:", email_list)
 
         for i in range(len(email_list)):
+            
             topic.add_subscription(
                 sns_subscriptions.EmailSubscription(Fn.select(i, email_list))
              )
